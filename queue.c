1691d925d8ea9ab32170fe98bf364caa2a33e610
CHI-WEI <zrs1475@gmail.com>
2025-03-11 04:58:53 +0800

    Implement q_delete_mid
    One node starts from the head to the previous node, and another to the
    next node
    
    Change-Id: I6b03ea8612f0fdfbfb411d6c3e16bb1b0dd57cad

--- queue.c
+++ queue.c
@@ -101,6 +101,16 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+    struct list_head *nprev = head->prev;
+    struct list_head *nnext = head->next;
+    while ((nprev != nnext) && (nnext->next != nprev)) {
+        nprev = nprev->prev;
+        nnext = nnext->next;
+    }
+    list_del(nprev);
+    q_release_element(list_entry(nprev, element_t, list));
     return true;
 }
 
